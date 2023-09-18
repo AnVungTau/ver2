dbea7ea061b1b63b3dec856f1439b8319e8e7aa5
anvungtau <anduy1572@gmail.com>
2023-09-18 20:35:11 +0700

    eh

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    // khai bao bien
+    // immutable val , mutable var
+
+
+
+    // kieu du lieu
+    var name: String = "an"
+    var weight = 55
+    var height = 1.75f
+    var age = 21
+    var isMale = true
+
+}
